ac4a11f6df105f593e3b7e45eb75df2c2eba2d1d
Greg <60129025+greg0rys@users.noreply.github.com>
2021-05-06 16:29:15 -0700

    Just playing with emoji output

---
+++ likeMe.py
@@ -0,0 +1,34 @@
+import emoji
+use_aliases=True
+print(emoji.emojize(' :thought_balloon:'))
+
+likeMe = input('Do you like me? Enter y/n')
+
+while likeMe == 'y':
+    print(emoji.emojize(' :sparkling_heart:,Daddy chill'))
+    likeMe = True
+    print('So that statement is:',likeMe)
+
+while likeMe == 'n':
+    print(emoji.emojize( ':unamused:', 'Bye this is a lie but what ever'))
+    likeMe = False
+    print('So then saying you like me would be', likeMe)
+    print(emoji.emojize(':fu:'))
+
+if likeMe:
+    print('Thank you so much for your support')
+    print('I am like Nicki Minaj')
+    print('& you are my barb')
+else:
+    print('Nobody wants you around here anyways. ')
+while likeMe == False:
+    print('You know you could change your mind! ')
+    print('You know I really am that bitch ')
+    print('Like just follow my example bro like i.e.')
+    print('I will run the world one day')
+    break
+while likeMe == True:
+    print('Girl, I know you\'re rich with no day job')
+    print(emoji.emojize('gayGang! :sparkling_heart:'))
+    print('p.s. all the rich ass boys \nwanna fuck on us', sep='')
+    break
